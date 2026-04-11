5e90b44278d46d54ff4fd20c860305e45e601cf1
Danil1310 <143375124+Danil1310@users.noreply.github.com>
2026-04-11 20:19:06 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,22 @@
-from .main import quadratic, linear_one_variable
+"""
+ENG
+---------
+
+A class that allows you to solve quadratic and linear equations.
+
+Methods:
+    linear_one_variable: solves the linear equation with one variable.
+    quadratic: solves the quadratic equation.
+
+RU
+---------
+
+Класс, позволяющий решать квадратные и линейные уравнения.
+
+Методы:
+    linear_one_variable: решает линейное уравнение с одной переменной.
+
+    quadratic: решает квадратное уравнение.
+"""
+
+from .main import linear_one_variable, quadratic
--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-"""
-ENG
----------
-
-A class that allows you to solve quadratic and linear equations.
-
-Methods:
-    linear_one_variable: solves the linear equation with one variable.
-    quadratic: solves the quadratic equation.
-
-RU
----------
-
-Класс, позволяющий решать квадратные и линейные уравнения.
-
-Методы:
-    linear_one_variable: решает линейное уравнение с одной переменной.
-
-    quadratic: решает квадратное уравнение.
-"""
-
 from math import sqrt
 from typing import Literal
 
